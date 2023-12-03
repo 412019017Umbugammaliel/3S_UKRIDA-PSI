efab317cacd567b937110ee0360ad27549b192b2
412019017Umbugammaliel <umbu.412019017@civitas.ukrida.ac.id>
2023-12-04 03:11:57 +0700

    4/12/2023

--- resources/views/userlogin/layouts/app.blade.php
+++ resources/views/userlogin/layouts/app.blade.php
@@ -15,6 +15,7 @@
   <link rel="stylesheet" href="{{ asset('/') }}dist/css/adminlte.min.css">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body class="sidebar-mini layout-fixed">
 <!-- Site wrapper -->
--- resources/views/userlogin/results.blade.php
+++ resources/views/userlogin/results.blade.php
@@ -8,6 +8,11 @@
             <div class="card-body">
                 <h3 class="text-center mb-4 font-weight-bold text-warning">Hasil Psikotes</h3>
 
+                @php
+                    $datasets = [];
+                    $labels = [];
+                @endphp
+
                 @foreach ($categories as $category)
                     <div class="card mb-4">
                         <div class="card-header bg-warning text-dark">
@@ -27,6 +32,12 @@
                                         <li>{{ $classification->title }} - {{ $classification->description }}</li>
                                     @endforeach
                                 </ul>
+
+                                @php
+                                    // Tambahkan dataset untuk setiap kategori
+                                    $datasets[] = $categoryPoint->total_points;
+                                    array_unshift($labels, $category->name_category);
+                                @endphp
                             @else
                                 <p>{{ __('No data available yet') }}</p>
                             @endif
@@ -34,7 +45,57 @@
                     </div>
                 @endforeach
 
-                <div class="text-center">
+                <!-- Tambahkan bagian untuk menampilkan grafik perbandingan -->
+                <div class="card">
+                    <div class="card-header bg-warning text-dark">
+                        Perbandingan Hasil Psikotes
+                    </div>
+                    <div class="card-body">
+                        <canvas id="comparisonChart" width="400" height="200"></canvas>
+                        <script>
+                            var ctxComparison = document.getElementById('comparisonChart').getContext('2d');
+                            var backgroundColors = ['rgba(201, 88, 169, 0.8)', 'rgba(255, 0, 0, 0.8)', 'rgba(28, 209, 208, 0.8)', 'rgba(255, 255, 0, 0.8)', 'rgba(27, 217, 178, 0.8)', 'rgba(165, 194, 216, 0.8)'];
+                        
+                            var comparisonChart = new Chart(ctxComparison, {
+                                type: 'bar',
+                                data: {
+                                    labels: @json($labels),
+                                    datasets: [{
+                                        backgroundColor: backgroundColors,
+                                        data: @json($datasets)
+                                    }]
+                                },
+                                options: {
+                                    scales: {
+                                        x: {
+                                            stacked: true
+                                        },
+                                        y: {
+                                            stacked: true,
+                                            beginAtZero: true
+                                        }
+                                    },
+                                    plugins: {
+                                        legend: {
+                                            position: 'top'
+                                        }
+                                    },
+                                    animation: {
+                                        duration: 1500,
+                                        easing: 'easeInOutQuart'
+                                    },
+                                    shadowOffsetX: 2,
+                                    shadowOffsetY: 2,
+                                    shadowBlur: 5,
+                                    shadowColor: 'rgba(0, 0, 0, 0.3)'
+                                }
+                            });
+                        </script>
+                        
+                    </div>
+                </div>
+
+                <div class="text-center mt-4">
                     <a href="{{ route('userlogin') }}" class="btn btn-primary">Kembali ke Beranda</a>
                 </div>
             </div>
